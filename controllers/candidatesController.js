b5474b88a4266dfb28c50b03fee0b0c3afa102ca
Kim Ngan <kimngan.ledang@gmail.com>
2020-12-16 15:02:07 +0100

    update controllers

--- controllers/candidatesController.js
+++ controllers/candidatesController.js
@@ -1,86 +1,93 @@
 const Candidate = require("../models/candidate");
+const User = require("../models/user");
+const app = require("express")();
 
 module.exports = {
-    index: (req, res, next) => {
-        Candidate.find({})
-            .exec()
-            .then((candidates) => {
-                res.locals.candidates = candidates;
-                next();
-            })
-            .catch((error) => {
-                console.log(`Error fetching candidates: ${error.message}`);
-                return [];
-            })
-            .then(() => {
-                console.log("promise complete");
-            });
-    },
-
-    indexView: (req, res) => {
-        res.render("candidates/index");
-    },
-
-    new: (req, res) => {
-        res.render("candidates/new");
-    },
-
-    //CREATE
-    create: (req, res, next) => {
-		let candidateParams = {
-            preferred_position: req.body.preferred_position,
-            soft_skills: req.body.soft_skills,
-            other_aspects: req.body.other_aspects,
-            work_culture_preferences: req.body.work_culture_preferences,
-        }
-		Candidate.create(candidateParams)
-			.then(candidate => {
-				res.locals.redirect = '/candidates';
-        res.locals.candidate = candidate;
-        console.log('a candidate saved into both MongoDB and Elasticsearch')
-				next();
-			})
-			.catch(error => {
-				console.log(`Error saving candidate profile: ${error.message}`);
-				next(error);
+  index: (req, res, next) => {
+    Candidate.find({})
+      .exec()
+      .then((candidates) => {
+        res.locals.candidates = candidates;
+        next();
+      })
+      .catch((error) => {
+        console.log(`Error fetching candidates: ${error.message}`);
+        return [];
+      })
+      .then(() => {
+        console.log("promise complete");
       });
-    },
+  },
 
-    redirectView: (req, res, next) => {
-        let redirectPath = res.locals.redirect;
-        if (redirectPath) res.redirect(redirectPath);
-        else next();
-    },
+  indexView: (req, res) => {
+    res.render("candidates/index");
+  },
 
-	show: (req, res, next) => {
-		let candidateId = req.params.id;
-		Candidate.findById(candidateId).then(candidate => {
-			res.locals.candidate = candidate;
-			next();
-		})
-			.catch(error => {
-				console.log(`Error fetching candidate by ID: ${error.message}`);
-				next(error);
-			});
-	},
+  new: (req, res) => {
+    res.render("candidates/new");
+  },
 
-	showView: (req, res) => {
-        res.render('candidates/show');
-    },
-  edit: (req,res,next) => {
+  create: (req, res, next) => {
+    let candidateParams = {
+      preferred_position: req.body.preferred_position,
+      soft_skills: req.body.soft_skills,
+      other_aspects: req.body.other_aspects,
+      work_culture_preferences: req.body.work_culture_preferences,
+    }
+    Candidate.create(candidateParams)
+      .then(candidate => {
+        req.flash('success', `${candidate.preferred_position} candidate created successfully!`);
+        res.locals.redirect = '/candidates';
+        res.locals.candidate = candidate;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error saving candidate profile: ${error.message}`);
+        res.locals.redirect = "/candidates/new";
+        req.flash(
+          "error",
+          `Failed to create user account because: ${error.message}.`
+        );
+        next();
+      });
+  },
+
+  redirectView: (req, res, next) => {
+    let redirectPath = res.locals.redirect;
+    if (redirectPath) res.redirect(redirectPath);
+    else next();
+  },
+
+  show: (req, res, next) => {
+    let candidateId = req.params.id;
+    Candidate.findById(candidateId).then(candidate => {
+      res.locals.candidate = candidate;
+      next();
+    })
+      .catch(error => {
+        console.log(`Error fetching candidate by ID: ${error.message}`);
+        next(error);
+      });
+  },
+
+  showView: (req, res) => {
+    res.render('candidates/show');
+  },
+  edit: (req, res, next) => {
 
     let candidateId = req.params.id;
     Candidate.findById(candidateId).then(candidate => {
       res.render("candidates/edit",
-      {
-        candidate: candidate
-      });
+        {
+          candidate: candidate
+        });
     })
       .catch(error => {
         console.log(`Error fetching user by ID: ${error.message}`);
         next(error);
       });
   },
+
   //UPDATE (WORKS)
   update: (req,res,next) => {
     let candidateId = req.params.id;
@@ -91,14 +98,12 @@ module.exports = {
       other_aspects: req.body.other_aspects,
       work_culture_preferences: req.body.work_culture_preferences,
     };
-    //has to be findOneAndUpdate bc it works with mongoosastic
-    Candidate.findOneAndUpdate({_id: candidateId},
-      {$set: candidateParams},
-      {new: true})
-    .then(candidate => {
-      res.locals.redirect = `/candidates/${candidateId}`;
-      res.locals.candidate = candidate;
-      next();
+
+    Candidate.findByIdAndUpdate(candidateId, { $set: candidateParams })
+      .then(candidate => {
+        res.locals.redirect = `/user/${req.app.locals.user._id}`;
+        res.locals.candidate = candidate;
+        next();
       })
       .catch(error => {
         console.log(`Error updating candidate by ID: ${error.message}`);
@@ -106,18 +111,16 @@ module.exports = {
       });
   },
 
-  //DELETE (WORKS)
   delete: (req, res, next) => {
     let candidateId = req.params.id;
     Candidate.findByIdAndRemove(candidateId)
-    .then(() => {
-      res.locals.redirect = "/candidates";
-      next();
-    })
-    .catch(error => {
-      console.log(`Error deleting candidate by ID: ${error.message}`);
-      next();
-    })
-  }
-
+      .then(() => {
+        res.locals.redirect = "/candidates";
+        next();
+      })
+      .catch(error => {
+        console.log(`Error deleting candidate by ID: ${error.message}`);
+        next();
+      })
+  },
 }
