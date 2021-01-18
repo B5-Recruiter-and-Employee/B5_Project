c579f8f15d89ce139d31c4e324398da72009b660
Ly Nguyen <mly.nguyen@yahoo.de>
2021-01-18 22:40:01 +0100

    a lot of fixes

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,6 @@
-const { db } = require("../models/job_offer");
 const Job = require("../models/job_offer");
 const User = require("../models/user");
+const userController = require("./userController");
 
 module.exports = {
 
@@ -17,9 +17,6 @@ module.exports = {
             .catch((error) => {
                 console.log(error.message);
                 return [];
-            })
-            .then(() => {
-                console.log("promise complete");
             });
     },
 
@@ -40,23 +37,14 @@ module.exports = {
 
     updateJob: (req, res) => {
         let jobId = req.params.jobId;
-        let user = req.user; //new
+        let jobParams = userController.getJobParams(req, res);
 
-        jobParams = {
-            job_title: req.body.job_title,
-            location: req.body.location,
-            salary: req.body.salary,
-            company_name: req.body.company_name,
-            description: req.body.description,
-        };
-    //  Job.findByIdAndUpdate(jobId, { $set: jobParams})
         Job.findOneAndUpdate({_id: jobId}, {$set: jobParams}, {new: true}, (err, job) => {
             if(err) {
                 req.flash('error', `There has been an error while updating the job offer: ${error.message}`);
                 console.log(`Error updating job by ID: ${error.message}`);
             } else {
-                //let user = res.locals.user;
-                req.flash('success', `The job has been successfully updated!`);
+                req.flash('success', `The job offer "${job.job_title}" has been successfully updated!`);
                 res.redirect(`/user/${user._id}/offers`);
                 console.log('job updated in MongoDB and Elasticsearch')
             }
@@ -91,45 +79,3 @@ module.exports = {
             })
     },
 }
-
-        // getAllJobs: (req, res) => {
-    //     Job.find({})
-    //         .exec()
-    //         .then((jobs) => {
-    //             res.render("jobs/index", {
-    //                 jobs: jobs
-    //             });
-    //         })
-    //         .catch((error) => {
-    //             console.log(error.message);
-    //             return [];
-    //         })
-    //         .then(() => {
-    //             console.log("promise complete");
-    //         });
-    // },
-  
-    // saveJob: (req, res) => {
-    //     let newJob = new Job({
-    //         job_title: req.body.job_title,
-    //         location: req.body.location,
-    //         salary: req.body.salary,
-    //         company_name: req.body.company_name,
-    //         description: req.body.description,
-    //     });
-    //     newJob.save()
-    //         .then(() => {
-    //             res.render('thanks', {
-    //                 flashMessages: { success: "The job has been created!"}
-    //             })
-    //             // req.flash('success', `The job offer has been created successfully!`);
-    //             // res.redirect(`thanks`);
-    //         })
-    //         .catch(error => {
-    //             res.send(error);
-    //         });
-    // },
-
-    // createJobs: (req, res) => {
-    //     res.render("jobs/new");
-    // },
