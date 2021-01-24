9898518903381ef6a6db40352196b406744431b9
mbareeva <beeemaja@gmail.com>
2021-01-24 21:32:12 +0100

    Fixes for cards/carousel

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -4,100 +4,110 @@ const User = require("../models/user");
 const userController = require("./userController");
 
 module.exports = {
-	renderSingleJobEdit: (req, res) => {
-		let jobId = req.params.jobId;
+  
+  /**
+   * Renders the edit view of one single job.
+   */
+  renderSingleJobEdit: (req, res) => {
+    let jobId = req.params.jobId;
 
-		Job.findOne({ _id: jobId })
-			.exec()
-			.then((job) => {
-				res.render("jobs/edit", {
-					job: job,
-				});
-			})
-			.catch((error) => {
-				console.log(error.message);
-				return [];
-			});
-	},
+    Job.findOne({ _id: jobId })
+      .exec()
+      .then((job) => {
+        res.render("jobs/edit", {
+          job: job,
+        });
+      })
+      .catch((error) => {
+        console.log(error.message);
+        return [];
+      });
+  },
 
-	renderSingleJob: (req, res, next) => {
-		let jobId = req.params.jobId;
+  /**
+   * Renders the view of one single job.
+   */
+  renderSingleJob: (req, res, next) => {
+    let jobId = req.params.jobId;
 
-		Job.findById(jobId)
-			.then((card) => {
-				User.find({ _id: card.user }).then((user) => {
-					let recruiterEmail;
-					if (user[0]) {
-						recruiterEmail = user[0].email;
-					}
-					res.render("jobs/showSingleJob", {
-						card: card,
-						email: recruiterEmail,
-					});
-					next();
-				});
-			})
-			.catch((error) => {
-				console.log(error.message);
-				return [];
-			});
-	},
+    Job.findById(jobId)
+      .then((card) => {
+        User.find({ _id: card.user }).then((user) => {
+          let recruiterEmail;
+          if (user[0]) {
+            recruiterEmail = user[0].email;
+          }
+          res.render("jobs/showSingleJob", {
+            card: card,
+            email: recruiterEmail,
+          });
+          next();
+        });
+      })
+      .catch((error) => {
+        console.log(error.message);
+        return [];
+      });
+  },
 
-	updateJob: (req, res) => {
-		let jobId = req.params.jobId;
-		let jobParams = userController.getJobParams(req, res);
+  /**
+     * Updates job of a recruiter.
+     */
+  updateJob: (req, res) => {
+    let jobId = req.params.jobId;
+    let jobParams = userController.getJobParams(req, res);
 
-		Job.findOneAndUpdate(
-			{ _id: jobId },
-			{ $set: jobParams },
-			{ new: true },
-			(err, job) => {
-				if (err) {
-					req.flash(
-						"error",
-						`There has been an error while updating the job offer: ${error.message}`
-					);
-					console.log(`Error updating job by ID: ${error.message}`);
-				} else {
-					req.flash(
-						"success",
-						`The job offer "${job.job_title}" has been successfully updated!`
-					);
-					res.redirect(`/user/${res.locals.user._id}/offers`);
-				}
-			}
-		);
-	},
+    Job.findOneAndUpdate(
+      { _id: jobId },
+      { $set: jobParams },
+      { new: true },
+      (err, job) => {
+        if (err) {
+          req.flash(
+            "error",
+            `There has been an error while updating the job offer: ${error.message}`
+          );
+          console.log(`Error updating job by ID: ${error.message}`);
+        } else {
+          req.flash(
+            "success",
+            `The job offer "${job.job_title}" has been successfully updated!`
+          );
+          res.redirect(`/user/${res.locals.user._id}/offers`);
+        }
+      }
+    );
+  },
 
-	/**
-	 * Delete the job offer from the whole jobs collection &
-	 * from the collection of job offers (jobOffers array in User) create by particular user.
-	 */
-	deleteJob: (req, res) => {
-		let jobId = req.params.jobId;
-		let user = req.user;
+  /**
+   * Delete the job offer from the whole jobs collection &
+   * from the collection of job offers (jobOffers array in User) create by particular user.
+   */
+  deleteJob: (req, res) => {
+    let jobId = req.params.jobId;
+    let user = req.user;
 
-		Job.findOneAndRemove({ _id: jobId }).then((job) => {
-			console.log("user to update:", user);
-			// delete job from user's jobs array (both in MongoDB and ES)
-			User.findOneAndUpdate(
-				{ _id: user._id },
-				{ $pull: { jobOffers: job._id } },
-				{ new: true }
-			)
-				.then((user) => {
-					console.log("job deleted from the jobOffers Array");
-					req.flash("success", `The job offer has been deleted successfully!`);
-					res.redirect(`/user/${user._id}/offers`);
-				})
-				.catch((error) => {
-					req.flash(
-						"error",
-						`There has been an error while deleting the job offer: ${error.message}`
-					);
-					console.log(`Error deleting job by ID: ${error.message}`);
-					next(error);
-				});
-		});
-	},
+    Job.findOneAndRemove({ _id: jobId }).then((job) => {
+      console.log("user to update:", user);
+      // delete job from user's jobs array (both in MongoDB and ES)
+      User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { jobOffers: job._id } },
+        { new: true }
+      )
+        .then((user) => {
+          console.log("job deleted from the jobOffers Array");
+          req.flash("success", `The job offer has been deleted successfully!`);
+          res.redirect(`/user/${user._id}/offers`);
+        })
+        .catch((error) => {
+          req.flash(
+            "error",
+            `There has been an error while deleting the job offer: ${error.message}`
+          );
+          console.log(`Error deleting job by ID: ${error.message}`);
+          next(error);
+        });
+    });
+  },
 };
