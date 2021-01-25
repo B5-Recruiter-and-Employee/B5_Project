d8a4a7e0955b241570c8e6016a8fac1ae2ad835c
Ly Nguyen <mly.nguyen@yahoo.de>
2021-01-25 17:48:30 +0100

    changes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,23 +2,27 @@ const httpStatus = require("http-status-codes");
 
 //catch requests made with no matching routes
 exports.respondNotFound = (req, res) => {
-    let errorCode = httpStatus.StatusCodes.NOT_FOUND;
-    res.status(errorCode);
-    res.send(`${errorCode} | The page doesn't exist! `);
+  let errorCode = httpStatus.StatusCodes.NOT_FOUND;
+  res.status(errorCode);
+  res.send(`${errorCode} | The page doesn't exist! `);
 };
 
 //catch requests where internal errors occurred
 exports.respondInternalError = (req, res, next) => {
-    let errorCode = httpStatus.StatusCodes.INTERNAL_SERVER_ERROR;
-    res.status(errorCode);
-    res.send(`${errorCode} | Sorry, our application is experiencing a problem!`);
+  let errorCode = httpStatus.StatusCodes.INTERNAL_SERVER_ERROR;
+  res.status(errorCode);
+  res.send(`${errorCode} | Sorry, our application is experiencing a problem!`);
 };
 
-exports.respondAccessDenied = (req, res, next, redirect) => {
-    let errorCode = httpStatus.StatusCodes.NETWORK_AUTHENTICATION_REQUIRED;
-    res.status(errorCode);
-    req.flash("error", "You must be logged in to access the requested page.");
-    let path = "/user/login";
-    if (typeof redirect !== 'undefined') path += "?redirect=" + encodeURI(redirect);
-    res.redirect(path);
+// access denied function here
+
+// redirect to login when user is not logged in.
+// @param redirect: String of the relative path you want to redirect the after login.
+exports.respondNotLoggedin = (req, res, next, redirect) => {
+  let errorCode = httpStatus.StatusCodes.NETWORK_AUTHENTICATION_REQUIRED;
+  res.status(errorCode);
+  req.flash("error", "You must be logged in to access the requested page.");
+  let path = "/user/login";
+  if (typeof redirect !== 'undefined') path += "?redirect=" + encodeURI(redirect);
+  res.redirect(path);
 }
--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,12 +1,7 @@
 const homeController = require('../controllers/homeController');
 const router = require('express').Router();
 
-//create route to home page and log requests body
-//(send data with a curl command, just for practice)
-router.post("/", homeController.logRequestPaths);
-
 //route to index page
 router.get("/", homeController.getIndex);
 
 module.exports = router;
-
