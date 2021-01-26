95e2741edb19a0f5d38cb07ac19d844b146d0973
Ly Nguyen <mly.nguyen@yahoo.de>
2021-01-26 19:20:06 +0100

    access denied error added

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,27 +2,32 @@ const httpStatus = require("http-status-codes");
 
 //catch requests made with no matching routes
 exports.respondNotFound = (req, res) => {
-  let errorCode = httpStatus.StatusCodes.NOT_FOUND;
-  res.status(errorCode);
-  res.send(`${errorCode} | The page doesn't exist! `);
+    let errorCode = httpStatus.StatusCodes.NOT_FOUND;
+    res.status(errorCode);
+    res.send(`${errorCode} | The page doesn't exist! `);
 };
 
 //catch requests where internal errors occurred
 exports.respondInternalError = (req, res, next) => {
-  let errorCode = httpStatus.StatusCodes.INTERNAL_SERVER_ERROR;
-  res.status(errorCode);
-  res.send(`${errorCode} | Sorry, our application is experiencing a problem!`);
+    let errorCode = httpStatus.StatusCodes.INTERNAL_SERVER_ERROR;
+    res.status(errorCode);
+    res.send(`${errorCode} | Sorry, our application is experiencing a problem!`);
 };
 
 // access denied function here
+exports.respondAccessDenied = (req, res, next) => {
+    let errorCode = httpStatus.StatusCodes.UNAUTHORIZED;
+    res.status(errorCode);
+    res.render("error/access-denied");
+}
 
 // redirect to login when user is not logged in.
 // @param redirect: String of the relative path you want to redirect the after login.
 exports.respondNotLoggedin = (req, res, next, redirect) => {
-  let errorCode = httpStatus.StatusCodes.NETWORK_AUTHENTICATION_REQUIRED;
-  res.status(errorCode);
-  req.flash("error", "You must be logged in to access the requested page.");
-  let path = "/user/login";
-  if (typeof redirect !== 'undefined') path += "?redirect=" + encodeURI(redirect);
-  res.redirect(path);
+    let errorCode = httpStatus.StatusCodes.NETWORK_AUTHENTICATION_REQUIRED;
+    res.status(errorCode);
+    req.flash("error", "You must be logged in to access the requested page.");
+    let path = "/user/login";
+    if (typeof redirect !== 'undefined') path += "?redirect=" + encodeURI(redirect);
+    res.redirect(path);
 }
--- routes/errorRouter.js
+++ routes/errorRouter.js
@@ -2,6 +2,5 @@ const errorController = require('../controllers/errorController');
 var router = require('express').Router();
 
 router.use(errorController.respondNotFound);
-router.use(errorController.respondInternalError);
 
 module.exports = router;
